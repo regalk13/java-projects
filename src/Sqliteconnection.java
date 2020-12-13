4bc5efa34dbd2ad19bc4903ed5fc7ba2e5781b3f
regalk13 <jsalazarcaviedes30@gmail.com>
2020-12-12 21:41:10 -0500

    {Fix} Database Problem

--- src/Sqliteconnection.java
+++ src/Sqliteconnection.java
@@ -1,17 +1,23 @@
 
 package frame1;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+
 
 public class Sqliteconnection {
   public static Connection connect() {
     Connection con = null;
+    Statement stmt = null;
     try {
       Class.forName("org.sqlite.JDBC");
       con = DriverManager.getConnection("jdbc:sqlite:data.db"); // connecting to our database
       System.out.println("Connected!");
-   
+      stmt = con.createStatement();
+      String sql = "CREATE TABLE IF NOT EXISTS Stocks " +
+                        "(name TEXT PRIMARY KEY     NOT NULL," +
+                        " price           TEXT    NOT NULL, " + 
+                        " stocks          TEXT     NOT NULL)";
+      stmt.executeUpdate(sql);
+      stmt.close();
     } catch (ClassNotFoundException | SQLException e ) {
       // TODO Auto-generated catch block
       System.out.println(e+"");
